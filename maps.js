18f5d06eed7a6b8add3e47288ffd656ad43732e9
ocitmas36-svg <ocitmas36@gmail.com>
2026-02-07 23:47:15 +0700

    Create maps.js

---
+++ maps.js
@@ -0,0 +1,15 @@
+const MapData = {
+    width: 5000,
+    height: 3000,
+    dividerX: 2500, // Garis tengah pembatas kota & desa
+    city: {
+        name: "KOTA JAYAKARTA",
+        groundColor: 0x333333, // Aspal Kelabu
+        lineColor: 0xffffff
+    },
+    village: {
+        name: "DESA BOJONG GEDE",
+        groundColor: 0x1b5e20, // Hijau Tua (Sawah/Kebun)
+        lineColor: 0x795548
+    }
+};
